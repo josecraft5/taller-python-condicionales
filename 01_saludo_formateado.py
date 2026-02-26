422655abf73f2143accde5c11d3fe6d0c6797365
estudiante <estudiante@usfq.edu.ec>
2026-02-26 07:37:27 -0500

    Se realizo el primer ejercicio saludo formateado, el programa fnciona correctamente y esta completo

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,6 @@
+# Saludo formateado
+nombre = str(input("Cual es tu nombre?: "))
+edad = int(input("Cuantos años tienes?: "))
+ciudad = str(input("De donde eres?: "))
+
+print(f"Hola {nombre}, tienes {edad} y vives en {ciudad}")
