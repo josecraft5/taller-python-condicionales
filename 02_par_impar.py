b23a386fa116af9210052b5ce2f82e96c62a397e
estudiante <estudiante@usfq.edu.ec>
2026-02-26 07:49:21 -0500

    Se finalizo el programa del ejercicio 2 funciona correctamente

---
+++ 02_par_impar.py
@@ -0,0 +1,16 @@
+numero = int(input("Dime un numero entero: "))
+edad = int(input("Dime tu edad: "))
+
+if numero % 2 == 0:
+    numero= "PAR"
+else:
+    numero= "IMPAR"
+
+
+if edad>=18:
+    edad= "MAYOR"
+else:
+    edad= "MENOR"
+
+print(f"Tu numero es {numero}")
+print(f"Y eres {edad} de edad")
